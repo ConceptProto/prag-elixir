3e9ad32fc24d57483d050dafbcea9c73684d09e8
Jacques De Waal <jacquesdewaal@Jacquess-MacBook-Air.local>
2024-02-19 22:24:07 +0200

    started GET /api/bears

--- lib/servy/wildthings.ex
+++ lib/servy/wildthings.ex
@@ -1,27 +1,30 @@
 defmodule Servy.Wildthings do
   alias Servy.Bear
 
-  def list_bears do
-    [
-      %Bear{id: 1, name: "Teddy", type: "Brown", hibernating: true},
-      %Bear{id: 2, name: "Smokey", type: "Black", hibernating: false},
-      %Bear{id: 3, name: "Paddington", type: "Brown", hibernating: false},
-      %Bear{id: 4, name: "Scarface", type: "Grizzly", hibernating: true},
-      %Bear{id: 5, name: "Snow", type: "Polar", hibernating: false},
-      %Bear{id: 6, name: "Brutus", type: "Grizzly", hibernating: false},
-      %Bear{id: 7, name: "Rosie", type: "Black", hibernating: true},
-      %Bear{id: 8, name: "Roscoe", type: "Panda", hibernating: false},
-      %Bear{id: 9, name: "Iceman", type: "Polar", hibernating: true},
-      %Bear{id: 10, name: "Kenai", type: "Grizzly", hibernating: false}
-    ]
-  end
-
   # def list_bears do
-  #   Path.expand("../../db", __DIR__)
-  #   TODO: Path.join
-  #   TODO: some more things here
-  #   Poison.decode!(json, as: %{"bears" => [%Bear{}]})
+  #   [
+  #     %Bear{id: 2, name: "Smokey", type: "Black", hibernating: false},
+  #     %Bear{id: 3, name: "Paddington", type: "Brown", hibernating: false},
+  #     %Bear{id: 4, name: "Scarface", type: "Grizzly", hibernating: true},
+  #     %Bear{id: 5, name: "Snow", type: "Polar", hibernating: false},
+  #     %Bear{id: 6, name: "Brutus", type: "Grizzly", hibernating: false},
+  #     %Bear{id: 7, name: "Rosie", type: "Black", hibernating: true},
+  #     %Bear{id: 1, name: "Teddy", type: "Brown", hibernating: true},
+  #     %Bear{id: 8, name: "Roscoe", type: "Panda", hibernating: false},
+  #     %Bear{id: 9, name: "Iceman", type: "Polar", hibernating: true},
+  #     %Bear{id: 10, name: "Kenai", type: "Grizzly", hibernating: false}
+  #   ]
   # end
+
+  def list_bears do
+    path = Path.expand("../../db", __DIR__)
+    # file = Path.join()
+    # TODO: some more things here
+
+    file = Code.eval_file("bears.json", path)
+    json = Poison.decode!(file, as: %{"bears" => [%Bear{}]})
+    IO.inspect(json, label: "list_bears json 🚧")
+  end
 
   # TODO: Seems like I need to read the json as well
 
@@ -35,3 +38,5 @@ defmodule Servy.Wildthings do
     |> Enum.find(fn bear -> bear.id == String.to_integer(id) end)
   end
 end
+
+# Poison.decode!("{"bears": [{"id": 1,"name": "Teddy","type": "Brown","hibernating": true}]}", as: %{"bears" => [%Bear{}]})
